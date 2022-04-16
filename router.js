500dc90e11541dd953387dc4fba0dbf231393db2
lawal rashidi <lawalrashidi95@gmail.com>
2022-04-16 12:40:27 +0100

    updated and complete

--- router.js
+++ router.js
@@ -3,28 +3,46 @@ var router = express.Router();
 
 
 const credential = {
-    email: "admin@gmail.com",
-    password:"admin123"
+    email: "rashid@gmail.com",
+    password: "law12"
 }
+
 
 
 //login user
 router.post('/login', (req, res) => {
-    if(req.body.email == credential.email){
+    if(req.body.email == credential.email && req.body.password == credential.password){
        req.session.user = req.body.email;
-    //    res.redirect('/dashboard')
-       res.end(("Login Successful"))
+       res.redirect('/route/dashboard')
+    //res.end(("Login Successful"))
     } else {
-        res.end('Invalid Username')
+        res.end('Invalid Username or Password')
     }
     
-    if(req.body.password == credential.password){
-        req.session.user = req.body.password;
-        // res.redirect('dashboard')
-        res.end(("Login Successful"))
+});
+
+
+//route for dashboard
+router.get('/dashboard', (req,res) => {
+    if(req.session.user){
+        res.render('dashboard', {user: req.session.user})
     }else{
-        res.end('Invalid Username')
+        res.send("Unauthorized User")
     }
 })
 
-module.exports = router
+
+//route for logout
+router.get('/logout', (req, res) => {
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+            res.send("Error")
+        }else{
+            res.render('base',{title:"Express", logout:"Logout Successfully"})
+        }
+    })
+})
+
+
+module.exports = router;
